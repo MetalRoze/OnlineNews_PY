222a97d43c445c235007d25dd0f64ea0fc6552e1
eunnnju <sbsdy3@gmail.com>
2024-11-08 00:35:26 +0900

    setting

---
+++ app/__init__.py
@@ -0,0 +1,4 @@
+# app/__init__.py
+
+# main.py 모듈을 이미 import해 두면, `import app`만으로 main을 사용할 수 있게 됩니다.
+from .main import app
--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
+# app/main.py
 from fastapi import FastAPI
-from app.my_code import my_function  # 실행하고 싶은 파이썬 코드
+from fastapi.responses import FileResponse
+import os
 
 app = FastAPI()
 
-@app.get("/run_code")
-def run_code():
-    result = my_function()  # 외부 파이썬 코드 실행
-    return {"result": result}
+# favicon.ico 파일을 제공할 경로 설정
+@app.get("/favicon.ico")
+async def favicon():
+    return FileResponse(os.path.join("app", "static", "favicon.ico"))
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the root endpoint!"}
