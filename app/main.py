b0d358c81381cf3b7976e4b349ff0a999fd95df8
eunnnju <sbsdy3@gmail.com>
2024-11-25 21:42:04 +0900

    cors 설정

--- app/main.py
+++ app/main.py
@@ -1,11 +1,26 @@
 # app/main.py
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 from app.my_code import process_article
 import os
 import uvicorn
 
 app = FastAPI()
+
+origins = [
+        "http://myeongbo.site",
+]
+
+# CORS 설정 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # 허용할 출처
+    allow_credentials=True,  # 쿠키 허용 여부
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용 (GET, POST 등)
+    allow_headers=["*"],  # 모든 HTTP 헤더 허용
+)
+
 
 # favicon.ico 파일을 제공할 경로 설정
 @app.get("/favicon.ico")
@@ -31,4 +46,5 @@ def run_my_code(article_id: int):
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    
 
